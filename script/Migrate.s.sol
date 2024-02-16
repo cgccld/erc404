93c9e3c4cfb255cd08285244a267444a42fc0f9e
tasibii <tasibii.web3@gmail.com>
2024-02-16 13:01:52 +0700

    temporary commit

--- script/Migrate.s.sol
+++ script/Migrate.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.23;
+pragma solidity ^0.8.20;
 
 import {console2 as console, StdStyle, BaseDeploy} from "@kit/BaseDeploy.s.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
